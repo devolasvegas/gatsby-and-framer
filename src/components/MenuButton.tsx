558c7ae545fd31b577cce87ed8974cabccb6ad17
Gel Studio <devondaviau@yahoo.ca>
2019-01-24 14:51:07 -0500

    initial commit

---
+++ src/components/MenuButton.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import { PropertyControls, ControlType } from "framer";
+
+const style: React.CSSProperties = {
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+    color: "#8855FF",
+    background: "rgba(136, 85, 255, 0.1)",
+    overflow: "hidden",
+    fontSize: "100px",
+};
+
+// Define type of property
+interface Props {
+    text: string;
+}
+
+export class MenuButton extends React.Component<Props> {
+
+    // Set default properties
+    static defaultProps = {
+    text: "Hello World!"
+    }
+
+    // Items shown in property panel
+    static propertyControls: PropertyControls = {
+    text: { type: ControlType.String, title: "Text" },
+    }
+
+    render() {
+    return <div style={style}>{this.props.text}</div>;
+    }
+}
